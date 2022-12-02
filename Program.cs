b08521e5e43d1fc3e342315141678ff8caff5301
Kurt Brewer <BREWERKM1@ETSU.EDU>
2022-12-02 01:03:35 -0500

    vfdgrbdc

--- Program.cs
+++ Program.cs
@@ -4,7 +4,60 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Calculator> calc = new();
+
+            Stack<List<Calculator>> history = new();
+
+            Dictionary<string, dynamic> menuActions = new();
+
+            bool isRunning = true;
+
+            menuActions["exit"] = new Action(() => { isRunning = false; });
+            menuActions["print"] = new Action(() => { PrintRoster(calc); Console.WriteLine("Press Enter/Return to continue..."); Console.ReadLine(); });
+            menuActions["add"] = new Action<string, string>((first, last) => { history.Push(calc.ToList()); calc.Add(new Calculator(first, last)); });
+            menuActions["delete"] = new Action<string, string>((first, last) => { history.Push(calc.ToList()); calc.Remove(calc.Find(x => x.FirstName == first && x.LastName == last)); });
+            menuActions["undo"] = new Action(() => history.TryPop(out calc));
+
+            while (isRunning)
+            {
+                // Display a menu options
+                Console.Clear();
+                Console.WriteLine("Add [firstName] [lastName]");
+                Console.WriteLine("Delete [firstName] [lastName]");
+                Console.WriteLine("Grade");
+                Console.WriteLine("Print");
+                Console.WriteLine("Undo");
+                Console.WriteLine("Exit");
+
+                // Ask for menu option
+                // Get menu option
+                string[] inputs = Console.ReadLine().Split(" ");
+                inputs[0] = inputs[0].ToLower();
+
+                // Do menu option
+                if (menuActions.ContainsKey(inputs[0]))
+                {
+                    if (inputs.Length == 1)
+                        menuActions[inputs[0]]();
+                    if (inputs.Length == 3)
+                        menuActions[inputs[0]](inputs[1], inputs[2]);
+                }
+            }
+        }
+
+        static void PrintRoster(List<Calculator> roster)
+        {
+            ConsoleColor oldbg, oldfg;
+            oldbg = Console.BackgroundColor;
+            oldfg = Console.ForegroundColor;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            foreach (var item in roster)
+            {
+                Console.WriteLine(item.FullName);
+            }
+            Console.BackgroundColor = oldbg;
+            Console.ForegroundColor = oldfg;
         }
     }
 }
